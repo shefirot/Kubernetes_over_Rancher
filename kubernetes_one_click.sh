0d7b471629c19baad072f1a585cf6e4e177a3276
shefirot <dgp.asir@gmail.com>
2017-01-27 10:17:23 +0100

    Add files via upload

--- kubernetes_one_click.sh
+++ kubernetes_one_click.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+#set -x
 #author David Gonzalez Pozo dgp.asir//@//gmail//.com
 #Mensaje de uso general donde se define la llamada al script
-#En caso de realizarlo en localhost no es necesario definir el usuario de ssh
 #Si no se define un usuario, por defecto se creara root Pass
 Uso="Uso: kubernetes_ono_click.sh 1:IP_rancher_server 2:IP_rancher_host [3:Usuario_rancher_server (por defecto root) 4:Clave_rancher_server (por defecto Pass) 5:Usuario_conexion_ssh(por defecto core)]"
 # $? devolvera mayor que 0 en caso de error
@@ -10,6 +10,7 @@ Uso="Uso: kubernetes_ono_click.sh 1:IP_rancher_server 2:IP_rancher_host [3:Usuar
 if [ -z $5 ]
   then
     echo "Establecido el usuario de ssh por defecto a core"
+    echo
     user="core"
   else
     user=$5
@@ -55,8 +56,9 @@ fi
 
 
 #Instalacion de docker de rancher
+echo
 echo -n "Creamos el docker de rancher server, tardara unos minutos"
-ssh $user@$1 sudo docker run --name rancher-server -d --restart=always -p 8080:8080 rancher/server:stable >> /dev/null
+ssh $user@$1 sudo docker run --name rancher-server -d --restart=always -p 8080:8080 rancher/server:stable &>> /dev/null
 if [ $? != 0 ]  
   then   
     echo "Error al instalar el docker de rancher" 
@@ -65,18 +67,20 @@ fi
 
 
 #Espera activa hasta que el servicio este disponible
-curl -s $iP_Puerto >> /dev/null
+curl -s $iP_Puerto &>> /dev/null
 until [  $? == 0 ]; do
   echo -n "."
   sleep 1s
-  curl -s $iP_Puerto >> /dev/null
+  curl -s $iP_Puerto &>> /dev/null
 done
-
+echo "."
 sleep 10s
 
 
+echo
 echo "Obtenemos el token de autenticacion"
-token=($(curl -X POST $iP_Puerto/v1/apikey | jq '. | .publicValue, .secretValue'))  >> /dev/null
+echo
+token=($(curl -X POST $iP_Puerto/v1/apikey | jq '. | .publicValue, .secretValue')) &>> /dev/null
 if [ $? != 0 ]
   then
     echo "Error al obtener el token de rancher" 
@@ -84,18 +88,22 @@ if [ $? != 0 ]
 fi
 
 
+echo
 echo "Borrar el entorno por defecto de cattle"
+echo
 curl -u "${token[0]}":"${token[1]}" \
 -X DELETE \
-$iP_Puerto/v1/projects/1a5 | jq . >> /dev/null
+$iP_Puerto/v1/projects/1a5 | jq . &>> /dev/null
 if [ $? != 0 ]
   then
     echo "Warning al borrar el entorno de cattle, sigue la ejecuccion." 
 fi
 
 
+echo
 echo "Obtenemos el id del template de kubernetes"
-envTemplate=($(curl -u "${token[0]}":"${token[1]}" $iP_Puerto/v2-beta/projecttemplates/ | jq '.data[] | select( .description=="Default Kubernetes template") | {idTemplateKuberntes: .id}'))  >> /dev/null
+echo
+envTemplate=($(curl -u "${token[0]}":"${token[1]}" $iP_Puerto/v2-beta/projecttemplates/ | jq '.data[] | select( .description=="Default Kubernetes template") | {idTemplateKuberntes: .id}'))  &>> /dev/null
 if [ $? != 0 ]
   then
     echo "Error al obtener el id del template de rancher" 
@@ -103,13 +111,15 @@ if [ $? != 0 ]
 fi
 
 
+echo
 echo "Creamos el enviroment"
+echo
 id_env=($(curl -u "${token[0]}":"${token[1]}" \
 -X POST \
 -H 'Accept: application/json' \
 -H 'Content-Type: application/json' \
 -d '{"description":"Entorno de pruebas de Kubernetes", "name":"K8s2", "projectTemplateId":'${envTemplate[2]}', "allowSystemRole":false, "members":[], "virtualMachine":false, "servicesPortRange":null}' \
-$iP_Puerto/v2-beta/projects/ | jq '. | .id, .data.fields.orchestration' ))  >> /dev/null
+$iP_Puerto/v2-beta/projects/ | jq '. | .id, .data.fields.orchestration' )) &>> /dev/null
 if [ $? != 0 ]
   then
     echo "Error al crear el enviroment de kubernetes" 
@@ -118,9 +128,11 @@ fi
 
 
 #Hay que esperar unos segundos hasta que se cree correctamente el enviroment
+echo
 echo "Obtencion del comando para añadir cada host al enviroment, tardara unos segundos"
+echo
 sleep 5s
-subEnviroment=($(curl -X POST -u "${token[0]}":"${token[1]}" $iP_Puerto/v1/registrationtokens?projectId=$(echo "${id_env[0]}" | sed 's|"||g') | jq ' .| {idSubEnviroment: .id}'))  >> /dev/null
+subEnviroment=($(curl -X POST -u "${token[0]}":"${token[1]}" $iP_Puerto/v1/registrationtokens?projectId=$(echo "${id_env[0]}" | sed 's|"||g') | jq ' .| {idSubEnviroment: .id}')) &>> /dev/null
 if [ $? != 0 ]
   then
     echo "Error al obtener el comando para añadir hosts" 
@@ -130,7 +142,7 @@ sleep 5s
 
 #quita las comillas dobles sobre el subenviroment
 aux=$(echo "${subEnviroment[2]}" | sed 's|"||g')
-comando=($(curl -u "${token[0]}":"${token[1]}" $iP_Puerto/v1/registrationtokens/$aux | jq ' .| .command'))  >> /dev/null
+comando=($(curl -u "${token[0]}":"${token[1]}" $iP_Puerto/v1/registrationtokens/$aux | jq ' .| .command'))   &>> /dev/null
 if [ $? != 0 ]
   then
     echo "Error al obtener el comando para añadir hosts" 
@@ -143,8 +155,10 @@ aux=$(echo "${comando[@]}" | sed 's|"||g')
 
 
 #Se añade el host al segundo equipo pasado por argumento
+echo
 echo "Añadimos el host al enviroment de kubernetes"
-ssh $user@$2 $aux  >> /dev/null
+echo
+ssh $user@$2 $aux   &>> /dev/null
 if [ $? != 0 ]
   then
     echo "Error al añadir el host" 
@@ -152,12 +166,16 @@ if [ $? != 0 ]
 fi
 
 
+echo
 echo "Creamos el usuario $userRancher con clave $claveRancher."
-curl -X POST -H 'Content-Type: application/json' $iP_Puerto/v1/localAuthConfig -d '{"type":"localAuthConfig","accessMode":"unrestricted","enabled":true,"name":'$userRancher',"username":'$userRancher',"password":'$claveRancher'}'
+echo
+curl -X POST -H 'Content-Type: application/json' $iP_Puerto/v1/localAuthConfig -d '{"type":"localAuthConfig","accessMode":"unrestricted","enabled":true,"name":'$userRancher',"username":'$userRancher',"password":'$claveRancher'}' &>> /dev/null
 if [ $? != 0 ]
   then
-    echo "Warning no se ha podido crear el usuario $userRancher con clave $claveRancher." 
+    echo "Warning no se ha podido crear el usuario $userRancher con clave $claveRancher."
+    echo
 fi
 
 
+echo
 echo "Comando para el resto de hosts: $aux"
--- kubernetes_one_click_localhost.sh
+++ kubernetes_one_click_localhost.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
+#set -x
 #author David Gonzalez Pozo dgp.asir//@//gmail//.com
 #Mensaje de uso general donde se define la llamada al script
+#En caso de realizarlo en localhost no es necesario definir el usuario de ssh
 #Si no se define un usuario, por defecto se creara root Pass
-Uso="Uso: kubernetes_ono_click.sh [3:Usuario_rancher_server (por defecto root) 4:Clave_rancher_server (por defecto Pass)]"
+Uso="Uso: kubernetes_ono_click_localhost.sh [1:Usuario_rancher_server (por defecto root) 2:Clave_rancher_server (por defecto Pass)]"
 # $? devolvera mayor que 0 en caso de error
+
 
 #Establece la IP el puerto para el rancher server
 iP_Puerto="127.0.0.1:8080"
 
 #Establece el usuario y la clave de rancher
-if [ -z $3 ]
+if [ -z $1 ]
   then
     userRancher='"root"'
   else
-    userRancher='"'$3'"'
+    userRancher='"'$1'"'
 fi
-if [ -z $4 ]
+if [ -z $2 ]
   then
     claveRancher='"Pass"'
   else
-    claveRancher='"'$4'"'
+    claveRancher='"'$2'"'
 fi
 
 
 #Instalacion de docker de rancher
+echo
 echo -n "Creamos el docker de rancher server, tardara unos minutos"
-sudo docker run --name rancher-server -d --restart=always -p 8080:8080 rancher/server:stable >> /dev/null
+sudo docker run --name rancher-server -d --restart=always -p 8080:8080 rancher/server:stable &>> /dev/null
 if [ $? != 0 ]  
   then   
     echo "Error al instalar el docker de rancher" 
@@ -34,18 +38,20 @@ fi
 
 
 #Espera activa hasta que el servicio este disponible
-curl -s $iP_Puerto >> /dev/null
+curl -s $iP_Puerto &>> /dev/null
 until [  $? == 0 ]; do
   echo -n "."
   sleep 1s
-  curl -s $iP_Puerto >> /dev/null
+  curl -s $iP_Puerto &>> /dev/null
 done
-
+echo "."
 sleep 10s
 
 
+echo
 echo "Obtenemos el token de autenticacion"
-token=($(curl -X POST $iP_Puerto/v1/apikey | jq '. | .publicValue, .secretValue'))  >> /dev/null
+echo
+token=($(curl -X POST $iP_Puerto/v1/apikey | jq '. | .publicValue, .secretValue')) &>> /dev/null
 if [ $? != 0 ]
   then
     echo "Error al obtener el token de rancher" 
@@ -53,18 +59,22 @@ if [ $? != 0 ]
 fi
 
 
+echo
 echo "Borrar el entorno por defecto de cattle"
+echo
 curl -u "${token[0]}":"${token[1]}" \
 -X DELETE \
-$iP_Puerto/v1/projects/1a5 | jq . >> /dev/null
+$iP_Puerto/v1/projects/1a5 | jq . &>> /dev/null
 if [ $? != 0 ]
   then
     echo "Warning al borrar el entorno de cattle, sigue la ejecuccion." 
 fi
 
 
+echo
 echo "Obtenemos el id del template de kubernetes"
-envTemplate=($(curl -u "${token[0]}":"${token[1]}" $iP_Puerto/v2-beta/projecttemplates/ | jq '.data[] | select( .description=="Default Kubernetes template") | {idTemplateKuberntes: .id}'))  >> /dev/null
+echo
+envTemplate=($(curl -u "${token[0]}":"${token[1]}" $iP_Puerto/v2-beta/projecttemplates/ | jq '.data[] | select( .description=="Default Kubernetes template") | {idTemplateKuberntes: .id}'))  &>> /dev/null
 if [ $? != 0 ]
   then
     echo "Error al obtener el id del template de rancher" 
@@ -72,13 +82,15 @@ if [ $? != 0 ]
 fi
 
 
+echo
 echo "Creamos el enviroment"
+echo
 id_env=($(curl -u "${token[0]}":"${token[1]}" \
 -X POST \
 -H 'Accept: application/json' \
 -H 'Content-Type: application/json' \
 -d '{"description":"Entorno de pruebas de Kubernetes", "name":"K8s2", "projectTemplateId":'${envTemplate[2]}', "allowSystemRole":false, "members":[], "virtualMachine":false, "servicesPortRange":null}' \
-$iP_Puerto/v2-beta/projects/ | jq '. | .id, .data.fields.orchestration' ))  >> /dev/null
+$iP_Puerto/v2-beta/projects/ | jq '. | .id, .data.fields.orchestration' )) &>> /dev/null
 if [ $? != 0 ]
   then
     echo "Error al crear el enviroment de kubernetes" 
@@ -87,9 +99,11 @@ fi
 
 
 #Hay que esperar unos segundos hasta que se cree correctamente el enviroment
+echo
 echo "Obtencion del comando para añadir cada host al enviroment, tardara unos segundos"
+echo
 sleep 5s
-subEnviroment=($(curl -X POST -u "${token[0]}":"${token[1]}" $iP_Puerto/v1/registrationtokens?projectId=$(echo "${id_env[0]}" | sed 's|"||g') | jq ' .| {idSubEnviroment: .id}'))  >> /dev/null
+subEnviroment=($(curl -X POST -u "${token[0]}":"${token[1]}" $iP_Puerto/v1/registrationtokens?projectId=$(echo "${id_env[0]}" | sed 's|"||g') | jq ' .| {idSubEnviroment: .id}')) &>> /dev/null
 if [ $? != 0 ]
   then
     echo "Error al obtener el comando para añadir hosts" 
@@ -99,7 +113,7 @@ sleep 5s
 
 #quita las comillas dobles sobre el subenviroment
 aux=$(echo "${subEnviroment[2]}" | sed 's|"||g')
-comando=($(curl -u "${token[0]}":"${token[1]}" $iP_Puerto/v1/registrationtokens/$aux | jq ' .| .command'))  >> /dev/null
+comando=($(curl -u "${token[0]}":"${token[1]}" $iP_Puerto/v1/registrationtokens/$aux | jq ' .| .command'))   &>> /dev/null
 if [ $? != 0 ]
   then
     echo "Error al obtener el comando para añadir hosts" 
@@ -112,8 +126,10 @@ aux=$(echo "${comando[@]}" | sed 's|"||g')
 
 
 #Se añade el host al segundo equipo pasado por argumento
+echo
 echo "Añadimos el host al enviroment de kubernetes"
-$aux  >> /dev/null
+echo
+$aux   &>> /dev/null
 if [ $? != 0 ]
   then
     echo "Error al añadir el host" 
@@ -121,12 +137,16 @@ if [ $? != 0 ]
 fi
 
 
+echo
 echo "Creamos el usuario $userRancher con clave $claveRancher."
-curl -X POST -H 'Content-Type: application/json' $iP_Puerto/v1/localAuthConfig -d '{"type":"localAuthConfig","accessMode":"unrestricted","enabled":true,"name":'$userRancher',"username":'$userRancher',"password":'$claveRancher'}'
+echo
+curl -X POST -H 'Content-Type: application/json' $iP_Puerto/v1/localAuthConfig -d '{"type":"localAuthConfig","accessMode":"unrestricted","enabled":true,"name":'$userRancher',"username":'$userRancher',"password":'$claveRancher'}' &>> /dev/null
 if [ $? != 0 ]
   then
-    echo "Warning no se ha podido crear el usuario $userRancher con clave $claveRancher." 
+    echo "Warning no se ha podido crear el usuario $userRancher con clave $claveRancher."
+    echo
 fi
 
 
+echo
 echo "Comando para el resto de hosts: $aux"
